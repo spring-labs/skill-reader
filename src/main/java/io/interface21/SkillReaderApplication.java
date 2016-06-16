06f66a9619a250a211a06894d0fd66d8fde12423
Heiko Scherrer <openwms@gmail.com>
2016-06-16 13:10:07 +0200

    skillreader

---
+++ src/main/java/io/interface21/SkillReaderApplication.java
@@ -0,0 +1,12 @@
+package io.interface21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillReaderApplication.class, args);
+	}
+}
